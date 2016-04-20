3f010a0a7bcc3b6fe51a1274248b40df2adfb75b
Steve Geluso <stevegeluso@gmail.com>
2016-04-20 16:31:11 -0700

    saving tweets to fake tweets

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -3,13 +3,14 @@ var db = require('../models');
 
 var router = express.Router();
 
+var fakeTweets = [
+  {username: 'spock', content: 'live long and prosper'},
+  {username: 'worf', content: 'FIRE!'},
+  {username: 'picard', content: 'engage'},
+  {username: 'picard', content: 'make it so'},
+];
+
 router.get('/', function(req, res) {
-	var fakeTweets = [
-	  {username: 'spock', content: 'live long and prosper'},
-	  {username: 'worf', content: 'FIRE!'},
-	  {username: 'picard', content: 'engage'},
-	  {username: 'picard', content: 'make it so'},
-	];
 	res.render('tweets', {tweets: fakeTweets});
 });
 
@@ -19,7 +20,9 @@ router.get('/new', function(req, res){
 
 router.post('/', function(req, res){
 	console.log(req.body);
-	res.send(req.body);
+	fakeTweets.push(req.body);
+
+	res.redirect('/tweets');
 });
 
 module.exports = router;
