939bc35ab25e7de616d57e7525d754be6996a8c7
Steve Geluso <stevegeluso@gmail.com>
2016-04-25 15:41:33 -0700

    moving auth routes to controller

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 
 // local dependencies 
 var tweetCtrl = require('./controllers/tweet');
+var authCtrl = require('./controllers/auth');
 var db = require('./models');
 
 var app = express();
@@ -39,54 +40,10 @@ app.use(function(req, res, next) {
 
 
 app.use('/tweets', tweetCtrl);
+app.use('/auth', authCtrl);
 
 app.get('/', function(req, res) {
   res.render('index', {alerts: req.flash()});
-});
-
-app.get('/auth/logout', function(req, res) {
-  req.currentUser = false;
-  res.locals.currentUser = false;
-  res.redirect('/');
-});
-
-app.post('/auth/signin', function(req, res) {
-  // proving we get the username and password
-  var user = req.body.username;
-  var pass = req.body.password;
-  db.user.authenticate(user, pass, function(err, user) {
-    // user successfully logged in.
-    if (user) {
-      req.session.userId = user.id;
-      req.flash('success', 'Successfully logged in.');
-      res.redirect('/tweets');
-    }
-  });
-});
-
-app.get('/auth/signup', function(req, res) {
-  res.render('signup', {alerts: req.flash()});
-});
-
-app.post('/auth/signup', function(req, res) {
-  db.user.findOrCreate({
-  	where: {
-  		username: req.body.username,
-  	},
-  	defaults: {
-  		password: req.body.password
-  	}
-  }).spread(function(user, isNew) {
-  	if (isNew) {
-    	res.redirect('/tweets');
-  	} else {
-  		req.flash('danger', 'Username already taken. Please choose another.')
-    	res.redirect('/auth/signup');
-  	}
-  }).catch(function(err) {
-    req.flash('danger', err.message);
-    res.redirect('/auth/signup')
-  });
 });
 
 var port = 3000;
