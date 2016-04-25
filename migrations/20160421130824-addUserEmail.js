b870f57838ab39c9b62f5b9bfca3cb72ad41aa29
Steve Geluso <stevegeluso@gmail.com>
2016-04-25 14:00:16 -0700

    removing extra user email column migration

---
+++ migrations/20160421130824-addUserEmail.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('users', 'email',
+        {type: Sequelize.STRING, defaultValue: 'unregistered@twitter.com'}
+    );
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
