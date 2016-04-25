070ce697fb80bd2d165c7541e1ea114f252fa712
Steve Geluso <stevegeluso@gmail.com>
2016-04-25 14:20:48 -0700

    hashing users passwords when creating accounts

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     username: DataTypes.STRING,
@@ -7,6 +9,22 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         models.user.hasMany(models.tweet);
+      }
+    }, 
+    hooks: {
+      beforeCreate: function(user, options, callback) {
+        // if the user gave a password then hash it
+        if (user.password) {
+          // hash the plaintext password before saving.
+          bcrypt.hash(user.password, 10, function(err, hash) {
+            if (err) return callback(err);
+            user.password = hash;
+            callback(null, user);
+          });
+        // the user didn't provide a password
+        } else {
+          callback(null, user);
+        }
       }
     }
   });
